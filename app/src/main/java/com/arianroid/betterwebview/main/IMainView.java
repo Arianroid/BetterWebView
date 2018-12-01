980c86cef1b963b521626e11e5c468900453c851
Arianroid <arian.tahmasebian@gmail.com>
2018-12-01 19:49:35 +0330

    Create main view

---
+++ app/src/main/java/com/arianroid/betterwebview/main/IMainView.java
@@ -0,0 +1,27 @@
+package com.arianroid.betterwebview.main;
+
+
+public interface IMainView {
+
+    void showMessage(int msgId);
+
+    void showInternetConnectionError();
+
+    void dismissInternetConnectionErrror();
+
+    void loadWebViewUrl();
+
+    void closeSplashDialog();
+
+    boolean isWebViewClientDataLoaded();
+
+    int getProgressBarValue();
+
+    void setProgressValue(int progress);
+
+    void showProgressbar();
+
+    void dissmisProgressbar();
+
+    void showTryAgain();
+}
