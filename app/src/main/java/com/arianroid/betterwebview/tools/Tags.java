ebc9e97e403c5790eed0c6b1fe73a17161fc29f8
Arianroid <arian.tahmasebian@gmail.com>
2018-12-01 19:35:41 +0330

    added tags

---
+++ app/src/main/java/com/arianroid/betterwebview/tools/Tags.java
@@ -0,0 +1,22 @@
+package com.arianroid.betterwebview.tools;
+
+
+public class Tags {
+
+    /************************* Main Page number Param *************************/
+    public static final int GAME_TYPE_PAGE_NUMBER = 0;
+    public static final int POPUP_TUTORIAL_STATE = 10;
+
+
+    /************************* Shared Pref tag key *************************/
+    public static final String TUTORIAL_POPUP_MUST_SHOW = "TUTORIALPOPUP";
+
+    /****************************  KEY Pair Dto Model *********************************/
+    public static final String MOBILE_VERSION = "MobileVersion";
+    public static final String FORCE_UPDATE = "ForceUpdate";
+
+    /*****************************  KEY Pair Notification data *****************************/
+    public static final String UPDATE_FORCE = "updateForce";
+    public static final String ORDER = "ORDER";
+
+}
